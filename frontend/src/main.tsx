8ae92fadc6a189a4dae0dff40cf9f3028959e0dc
Klavdija <klavdija.markovic.dev@gmail.com>
2026-07-08 17:37:55 +0200

    feat: initialize React frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render( /* creates a react root object */
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
