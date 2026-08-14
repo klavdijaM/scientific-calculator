9a9848ed7723e66260ba4f20ff2c34c3155228b4
Klavdija <klavdija.markovic.dev@gmail.com>
2026-08-14 18:27:18 +0200

    test(parser): add tests for parser precedence

--- backend/tests/test_parser.py
+++ backend/tests/test_parser.py
@@ -43,3 +43,106 @@ def test_parse_function():
     assert isinstance(ast.argument, NumberNode)
     assert ast.argument.value == "9"
 
+
+def test_unary_minus():
+    tokenizer = Tokenizer()
+    parser = Parser()
+
+    ast = parser.parse(tokenizer.tokenize("-5"))
+
+    assert isinstance(ast, UnaryOperationNode)
+    assert ast.operator == TokenType.MINUS
+
+    assert isinstance(ast.operand, NumberNode)
+    assert ast.operand.value == "5"
+
+
+def test_parse_addition():
+    tokenizer = Tokenizer()
+    parser = Parser()
+
+    ast = parser.parse(tokenizer.tokenize("2+3"))
+
+    assert isinstance(ast, BinaryOperationNode)
+    assert ast.operator == TokenType.PLUS
+
+    assert isinstance(ast.left, NumberNode)
+    assert ast.left.value == "2"
+
+    assert isinstance(ast.right, NumberNode)
+    assert ast.right.value == "3"
+
+
+def test_parse_operator_precedence():
+    tokenizer = Tokenizer()
+    parser = Parser()
+
+    ast = parser.parse(tokenizer.tokenize("2+3*4"))
+
+    assert isinstance(ast, BinaryOperationNode)
+    assert ast.operator == TokenType.PLUS
+
+    assert isinstance(ast.left, NumberNode)
+    assert ast.left.value == "2"
+
+    assert isinstance(ast.right, BinaryOperationNode)
+    assert ast.right.operator == TokenType.MULTIPLY
+
+    assert ast.right.left.value == "3"
+    assert ast.right.right.value == "4"
+
+
+def test_parse_parentheses():
+    tokenizer = Tokenizer()
+    parser = Parser()
+
+    ast = parser.parse(tokenizer.tokenize("(2+3)*4"))
+
+    assert isinstance(ast, BinaryOperationNode)
+    assert ast.operator == TokenType.MULTIPLY
+
+    assert isinstance(ast.left, BinaryOperationNode)
+    assert ast.left.operator == TokenType.PLUS
+
+    assert ast.left.left.value == "2"
+    assert ast.left.right.value == "3"
+
+    assert isinstance(ast.right, NumberNode)
+    assert ast.right.value == "4"
+
+
+def test_parse_power():
+    tokenizer = Tokenizer()
+    parser = Parser()
+
+    ast = parser.parse(tokenizer.tokenize("2^2^3"))
+
+    assert isinstance(ast, BinaryOperationNode)
+    assert ast.operator == TokenType.POWER
+
+    assert ast.left.value == "2"
+
+    assert isinstance(ast.right, BinaryOperationNode)
+    assert ast.right.operator == TokenType.POWER
+
+    assert ast.right.left.value == "2"
+    assert ast.right.right.value == "3"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+    
