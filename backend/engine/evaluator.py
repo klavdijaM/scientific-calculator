55c299c29a6a85a70f1998b808097e68efdfabc2
Klavdija <klavdija.markovic.dev@gmail.com>
2026-08-16 17:29:57 +0200

    feat(evaluator): evaluate number and constant nodes

--- backend/engine/evaluator.py
+++ backend/engine/evaluator.py
@@ -1,7 +1,20 @@
-from engine.ast import ASTNode
+from math import e, pi
+
+from engine.ast import ASTNode, NumberNode, ConstantNode
+
 
 class Evaluator:
     """Evaluates an abstract syntax tree."""
 
     def evaluate(self, node: ASTNode) -> float:
-        raise NotImplementedError
+        if isinstance(node, NumberNode):
+            return float(node.value)
+
+        elif isinstance(node, ConstantNode):
+            if node.value == "pi":
+                return pi
+            elif node.value == "e":
+                return e
+
+        raise ValueError(f"Unsupported node type: {type(node).__name__}")
+        
