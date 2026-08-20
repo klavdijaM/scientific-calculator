160dc4611397393fadffc387057338bff1c0b450
Klavdija <klavdija.markovic.dev@gmail.com>
2026-08-20 20:03:00 +0200

    feat(api): add calculation endpoint

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
-from api.schemas import CalculationRequest
+
+from api.schemas import CalculationRequest, CalculationResponse
+from engine.parser import Parser
+from engine.tokenizer import Tokenizer
+from engine.evaluator import Evaluator 
 
 router = APIRouter()
 
@@ -7,6 +11,15 @@ router = APIRouter()
 def hello():
     return {"message": "Hello from fastAPI"}
 
-@router.post("/calculate")
-def calculate(request: CalculationRequest):
-    return {"expression": request.expression}
+@router.post("/calculate", response_model=CalculationResponse)
+def calculate(request: CalculationRequest) -> CalculationResponse:
+
+    tokenizer = Tokenizer()
+    parser = Parser()
+    evaluator = Evaluator()
+
+    tokens = tokenizer.tokenize(request.expression)
+    ast = parser.parse(tokens)
+    result = evaluator.evaluate(ast)
+
+    return CalculationResponse(result=result)
